b6e9b7903814ea91537c037b19f259e352db8875
watsonpaul80 <76791685+watsonpaul80@users.noreply.github.com>
2025-03-30 23:10:25 +0100

    Update run.sh

--- minio/run.sh
+++ minio/run.sh
@@ -7,9 +7,9 @@ USERNAME=$(jq -r '.username' "$CONFIG_PATH")
 PASSWORD=$(jq -r '.password' "$CONFIG_PATH")
 PORT=$(jq -r '.port' "$CONFIG_PATH")
 CONSOLE_PORT=$(jq -r '.console_port' "$CONFIG_PATH")
+STORAGE_PATH=$(jq -r '.storage_path' "$CONFIG_PATH")
 GENERATE_KEYS=$(jq -r '.generate_keys' "$CONFIG_PATH")
 
-# Generate random credentials if enabled
 if [[ "$GENERATE_KEYS" == "true" && ( -z "$USERNAME" || -z "$PASSWORD" ) ]]; then
     echo "[INFO] Generating random credentials..."
     USERNAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
@@ -19,15 +19,10 @@ fi
 export MINIO_ROOT_USER="${USERNAME}"
 export MINIO_ROOT_PASSWORD="${PASSWORD}"
 
-# Optional: Set a browser redirect only if a proper URL can be formed
-if [[ -n "$INGRESS_ENTRY" ]]; then
-    export MINIO_BROWSER_REDIRECT_URL="http://homeassistant.local:8123${INGRESS_ENTRY}"
-    echo "[INFO] Auto-set MINIO_BROWSER_REDIRECT_URL to ${MINIO_BROWSER_REDIRECT_URL}"
-fi
-
 echo "[INFO] Starting MinIO in Ingress mode"
 echo "[INFO] Login with username: $USERNAME"
+echo "[INFO] Using storage path: $STORAGE_PATH"
 
-exec minio server /data \
+exec minio server "$STORAGE_PATH" \
   --address "127.0.0.1:${PORT}" \
   --console-address "127.0.0.1:${CONSOLE_PORT}"
